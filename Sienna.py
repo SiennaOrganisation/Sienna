a9dac4452e6dcaff5656d3ebae96415ae53ea05a
Crysaliux <114651917+Crysaliux@users.noreply.github.com>
2023-08-14 15:04:58 +0400

    Update Sienna.py

--- Sienna.py
+++ Sienna.py
@@ -151,7 +151,7 @@ async def on_application_command_error(ctx, error):
     else:
         raise error
 
-@bot.command(description="Sends the bot's latency.")
+@bot.slah_command(description="Sends the bot's latency.")
 async def ping(ctx):
     await ctx.respond(f"Pong! Latency is {bot.latency}")
 
